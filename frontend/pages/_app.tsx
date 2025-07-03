a7c544b7cc6346a229cfc587db19509d0213cb35
Your Name <you@example.com>
2025-07-03 18:47:53 +0700

    Re-add frontend files for deployment

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import '@/styles/globals.css'
+
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
---
+++ frontend/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from 'react-bootstrap'
+
+export default function Home() {
+  return (
+    <Container className="text-center mt-5">
+      <h1>🚀 Dự án Fullstack</h1>
+      <p>Đây là frontend dùng React Bootstrap</p>
+    </Container>
+  )
+}
