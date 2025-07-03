af98d7444239a390c0944a327d214abfdfabff03
Your Name <you@example.com>
2025-07-03 19:19:14 +0700

    test lan 1

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,16 @@
+import Link from 'next/link'
 import { Container } from 'react-bootstrap'
 
 export default function Home() {
   return (
     <Container className="text-center mt-5">
-      <h1>🚀 Dự án Fullstack</h1>
-      <p>Đây là frontend dùng React Bootstrap</p>
+      <h1>🏡 Trang chủ Fullstack</h1>
+      <p>Chào mừng bạn đến với website của Toàn.</p>
+
+      <div className="mt-4">
+        <Link href="/about" className="btn btn-primary mx-2">Giới thiệu</Link>
+        <Link href="/cv" className="btn btn-success mx-2">Xem CV</Link>
+      </div>
     </Container>
   )
 }
